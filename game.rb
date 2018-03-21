6f4dc88ed4dd9aa9330b243aa9a9a4bfa5bd1a19
Ethel Jerez <ethelmj@gmail.com>
2018-03-21 10:29:56 -0400

    updated game

--- game.rb
+++ game.rb
@@ -2,3 +2,13 @@ require './engine.rb'
 puts "Welcome to Wyncodiam"
 puts "Enter your name:"
 user_name = gets.chomp
+
+puts "Hi #{user_name}"!
+puts "Are you happy or sad?"
+user_feeling = gets.chomp
+
+If user_feeling == "happy"
+puts "Hooray!"
+elsif user_feeling == "sad"
+  puts "Deal with it!"
+end
