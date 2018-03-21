1060d336f58bd9e48f374e10011c6c1babe00550
Anthony Cook <antrafcook@gmail.com>
2018-03-21 10:29:29 -0400

    update game with feature

--- engine.rb
+++ engine.rb
@@ -1,4 +1,2 @@
 class GameEngine
-
-
 end
--- game.rb
+++ game.rb
@@ -1,4 +1,18 @@
 require './engine.rb'
 puts "Welcome to Wyncodiam"
-puts "Enter your name:"
+sleep(2)
+puts "Enter your name."
+sleep(2)
+puts "Hopefully it starts with the letter 'A'."
 user_name = gets.chomp
+if user_name[0] == ("A").downcase
+  puts "You got the juice now, #{user_name}"
+else
+  puts "#{user_name}, I DON'T. FUCK WITH. YOUUUU!"
+  sleep(3)
+  puts ("You little stupid ass bitch, I ain't fuckin' with you").upcase
+  sleep(3)
+puts ("You little, you little dumb ass bitch, I ain't fuckin' with you").upcase
+sleep(3)
+puts ("I got a million trillion things I'd rather fuckin' do, then to be fuckin' with you").upcase
+end
